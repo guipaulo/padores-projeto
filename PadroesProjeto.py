c0d7b324e43166a691ce5cc3e64f28781763b062
pauloguilherme <pauloguilherme_cn@outlook.com>
2022-08-02 15:49:23 -0300

    Create PadroesProjeto.py

---
+++ PadroesProjeto.py
@@ -0,0 +1,29 @@
+import os
+from pathlib import Path
+
+class Buscador:
+    def BuscarPDF():
+        contador = 0
+        palavra = str(input('Digite a palavra-chave: '))
+        for file in os.listdir("/home/paulo/Downloads"):
+            if file.endswith(".pdf"):
+                file_name = Path(file).stem
+                if palavra in file_name:
+                    print(os.path.join(file_name))
+                    print(' ')
+                    contador+=1
+        print(f'Foram encontrados {contador} arquivos!')
+
+class Interface():
+    pass
+
+class CadastrarArquivos():
+    pass
+
+class Facade:
+    '''Fachada com os subsistemas'''
+    def __init__(self):
+        self.buscador = Buscador.BuscarPDF()
+
+if __name__ == "__main__":
+    Aplicacao = Facade()
