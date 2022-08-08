b546efc0b3c2f0aa19889a6ce52fee775033ad04
pauloguilherme <pauloguilherme_cn@outlook.com>
2022-08-07 22:03:52 -0300

    Update PadroesProjeto.py

--- PadroesProjeto.py
+++ PadroesProjeto.py
@@ -5,13 +5,13 @@ from pymongo import MongoClient
 class Facade():
     '''Fachada com os subsistemas'''
     def __init__(self):
-        self.buscador = Buscador.BuscarPDF()
-        self.CadastrarArquivos()
+        palavra = str(input('Digite a palavra-chave: '))
+        self.buscador = Buscador.BuscarPDF(palavra)
+        #self.CadastrarArquivos()
 
 class Buscador():
-    def BuscarPDF():
+    def BuscarPDF(palavra):
         contador = 0
-        palavra = str(input('Digite a palavra-chave: '))
         for file in os.listdir("/home/paulo/Downloads"):
             if file.endswith(".pdf"):
                 file_name = Path(file).stem
@@ -20,15 +20,11 @@ class Buscador():
                     print(' ')
                     contador+=1
 
-class Interface():
+class InterfaceGUI():
     pass
 
 class CadastrarArquivos():
-    client = MongoClient('mongodb://localhost:8000')
-    banco = client.crawler_db
-    dados_crawler = banco.dados_crawler
-    documento1 = Buscador.BuscarPDF()
-    dados_crawler.insert_one(documento1)
+    pass
 
 if __name__ == "__main__":
     Aplicacao = Facade()
