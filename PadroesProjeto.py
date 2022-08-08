a33ebd7f0c874f25bd2bf5de0245dd36d908f533
pauloguilherme <pauloguilherme_cn@outlook.com>
2022-08-08 12:42:30 -0300

    Update PadroesProjeto.py

--- PadroesProjeto.py
+++ PadroesProjeto.py
@@ -5,17 +5,13 @@ import pymongo
 class Facade():
     '''Fachada com os subsistemas'''
     def __init__(self):
-        cliente = pymongo.MongoClient("mongodb://localhost:27017/")
-        BancoDados = cliente['banco_de_dados']
-        colecao = BancoDados['documentos']
-
         self.insert_arq = Arquivos.Inserir('1', 'Arquivo exemplo')
         palavra = str(input('Digite a palavra-chave: '))
         self.buscador = Buscador.BuscarPDF(palavra)
 
 class Buscador():
     def BuscarPDF(palavra):
-        for item in Facade.colecao.find({'text': palavra}):
+        for item in colecao.find({'text': palavra}):
             print(item)
 
 class InterfaceGUI():
@@ -24,10 +20,13 @@ class InterfaceGUI():
 class Arquivos(Facade):
     def Inserir(id = '', text = ''):
         documento = {"id": id, "texto": text}
-        d = Facade.colecao.insert_one(documento)
+        d = colecao.insert_one(documento)
     def Remover(id = ''):
         documento = {"id ": id}
-        Facade.colecao.delete_one(documento)
+        colecao.delete_one(documento)
 
 if __name__ == "__main__":
+    cliente = pymongo.MongoClient("mongodb://localhost:27017/")
+    BancoDados = cliente['banco_de_dados']
+    colecao = BancoDados['documentos']
     Aplicacao = Facade()
