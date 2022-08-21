6726aa5e3d4fc8cdfdf97970722be5d999962d43
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2022-08-21 20:30:43 -0300

    Update remover.py

--- Versao2/remover.py
+++ Versao2/remover.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python
 # -*- coding: latin-1 -*-
-import os, pathlib
-#from observer import ConcreteObserverA
+import os
 
 class Removedor:
     def Remover():
         filename = str(input('Qual arquivo deseja remover? '))
         user = os.getlogin()
-        file = f'C:\\Users\\{user}\\Documentos\\{filename}'
+        file = f'C:\\Users\\{user}\\Repositorio\\{filename}'
 
-        try:
-            file.unlink()
-        except OSError as e:
-            print(f"Error:{ e.strerror}")
+        if os.path.exists(file):
+            os.remove(file)
+            return True
+        else:
+            print('O arquivo nao existe!')
 Removedor.Remover()
