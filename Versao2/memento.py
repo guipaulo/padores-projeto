ff3c9d87e29d18ae2f97684b312fa9b0dade063e
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2022-08-21 21:15:53 -0300

    Create memento.py

---
+++ Versao2/memento.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+
+import os
+import shutil
+
+user = os.getlogin()
+class Memento:   
+    def ExcluiMemento(filename):
+        shutil.copy2 (f'C:\\Users\\{user}\\Repositorio\\{filename}', f'C:\\Users\\{user}\\Memento')
+        os.remove(f'C:\\Users\\{user}\\Repositorio\\{filename}')
+
+    def RetornaMemento(filename):
+        shutil.copy2 (f'C:\\Users\\{user}\\Memento\\{filename}', f'C:\\Users\\{user}\\Repositorio')
+        os.remove(f'C:\\Users\\{user}\\Memento\\{filename}')
+
+    def ExcluiPermanente(filename):
+        os.remove(f'C:\\Users\\{user}\\Memento\\{filename}')
+
+#Memento.ExcluiMemento('arquivo.txt')
+Memento.ExcluiPermanente('arquivo.txt')
